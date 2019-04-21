6ccab3fe22b967fe02d0ff36582bb6db2b726dd4
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-04-21 15:24:32 +0530

    async-await api calls"

--- client/javascripts/modules/getAPIstatus.js
+++ client/javascripts/modules/getAPIstatus.js
@@ -1,21 +1,20 @@
+/* eslint-disable no-console */
 import axios from 'axios';
+
 const token = 'some_token_here';
 
-function getAPIstatus() {
-    axios({
+const getAPIstatus = async () => {
+    const response = await axios({
         method: 'get',
         url: '/api/v1/',
         timeout: 20000,
         headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err);
+            Authorization: `Bearer ${token}`,
+        },
     });
-}
+    if (response) {
+        console.log(response.data);
+    }
+};
 
 export default getAPIstatus;
