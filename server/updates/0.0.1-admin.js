7c049ff5fcc0481aa56dc7953808f13f5d666e1b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-24 20:12:06 +0530

    server: read initial default user credentials from env file

--- server/updates/0.0.1-admin.js
+++ server/updates/0.0.1-admin.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-multi-assign, new-cap */
 const keystone = require('keystone');
 
+// import env variables
+require('dotenv').config({ path: 'variables.env' });
+
 const User = keystone.list('User');
 
 exports = module.exports = function(done) {
     new User.model({
-        name: { first: 'abhijith', last: 'vijayan' },
-        email: 'admin@abhijithvijayan.in',
-        password: 'abhijithvijayan',
+        name: { first: process.env.DEFAULT_ADMIN_FNAME, last: process.env.DEFAULT_ADMIN_LNAME },
+        email: process.env.DEFAULT_ADMIN_EMAIL,
+        password: process.env.DEFAULT_ADMIN_PASSWORD,
         canAccessKeystone: true,
     }).save(done);
 };
